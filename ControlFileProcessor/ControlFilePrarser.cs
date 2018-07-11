81716bae772c2cddd0a5ef27ada05c5b9913ffb9
Ranjan Dailata <rdailata@intry.careers>
2018-07-11 18:49:08 -0500

    Initial Commit

---
+++ ControlFileProcessor/ControlFilePrarser.cs
@@ -0,0 +1,60 @@
+﻿using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ControlFileProcessor
+{
+    public class ControlFileParser
+    {
+        private string controlFilePath;
+        private int tableNameLinePosition = 1;
+        private int columnNameLinePosition = 6;
+        private List<string> controlFileContents = new List<string>();
+
+        public ControlFileParser(string _controlFilePath)
+        {
+            controlFilePath = _controlFilePath;
+            if (File.Exists(controlFilePath))
+            {
+                controlFileContents = File.ReadAllText(controlFilePath)
+                    .Split('\n').ToList();
+            }
+
+        }
+
+        // Process Table Name
+        public string GetTableName()
+        {
+            if(controlFileContents.Count > 0)
+            {
+                var splittedTableLine = 
+                       controlFileContents[tableNameLinePosition]
+                       .Trim()
+                       .Split(' ');
+                if(splittedTableLine.Length == 3)
+                {
+                    return splittedTableLine[2].Replace("_BSTG", "");
+                }
+            }
+
+            return string.Empty;
+        }
+
+        // Process Column Names
+        public List<string> GetColumnNames()
+        {
+            var columnNames = new List<string>();
+            var columnLines = controlFileContents
+                    .Skip(columnNameLinePosition - 1) // Skip columns until we reach the column name line
+                    .ToList();
+            columnLines.RemoveAt(columnLines.Count - 1); // Remove the last line ')'
+            foreach (var columnLine in columnLines)
+            {
+                var tempColumnLine = columnLine.Trim().Replace(",", "");
+                var realColumnNames = tempColumnLine.Trim().Split(' ');
+                columnNames.Add(realColumnNames[0]);
+            }
+            return columnNames;
+        }
+    }
+}
---
+++ ControlFileProcessor/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Collections.Generic;
+
+namespace ControlFileProcessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exeFilePath = Path.GetDirectoryName(
+                Assembly.GetExecutingAssembly().Location);
+
+            var controlFolderPath = string.Format("{0}\\{1}",
+                exeFilePath, "ControlFiles");
+
+            DirectoryInfo d = new DirectoryInfo(controlFolderPath);
+            FileInfo[] Files = d.GetFiles("*.txt");
+
+            foreach (FileInfo file in Files)
+            {
+                var controlFileParser = new ControlFileParser(file.FullName);
+                var tableName = controlFileParser.GetTableName();
+                var columnNames = controlFileParser.GetColumnNames();
+
+                PrintTableName(tableName);
+                Console.WriteLine("\n");
+                PrintColumnNames(columnNames);
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void PrintTableName(string tableName)
+        {
+            Console.WriteLine("Table Name: "+ tableName);
+        }
+
+        private static void PrintColumnNames(List<string> columnNames)
+        {
+            Console.WriteLine("Column Names\n");
+            foreach (var columnName in columnNames)
+            {
+                Console.WriteLine(columnName);
+            }
+        }
+    }
+}
